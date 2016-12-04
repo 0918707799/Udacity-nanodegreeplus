d6b82c70e9efd204154d66fd7319ac1569f214a6
bittu072 <bittu.patel072@gmail.com>
2016-12-04 03:16:47 -0800

    implemented adding to list by input on webpage and display the list

--- multiuserblog/class_trial/trial_web/main.py
+++ multiuserblog/class_trial/trial_web/main.py
@@ -4,24 +4,8 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-
-hidden_html="""
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html="<li>%s</li>"
-
-shopping_html="""
-<br>
-<br>
-<br>
-<h2>Shopping list</h2>
-<ul>
-%s
-</ul>
-"""
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -34,32 +18,19 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+
 class MainPage(Handler):
     def get(self):
-        n = self.request.get("n")
-        if n:
-            n=int(n)
-        self.render("shopping_list.html", n=n)
-        # output = form_html
-        # output_hidden = ""
-        #
-        # items = self.request.get_all("food")
-        # if items:
-        #     output_items = ""
-        #     for item in items:
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-        #
-        #     output_shopping = shopping_html % output_items
-        #     output+=output_shopping
-        #
-        # output = output % output_hidden
-        # self.write(output)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
+
+
 class FizzBuzzHandler(Handler):
     def get(self):
         n = self.request.get("n", 0) # here 0 is for default value
         n = n and int(n)
         self.render('fizzbuzz.html', n=n)
+
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/fizzbuzz', FizzBuzzHandler)], debug=True)
