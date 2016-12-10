540b5422249c58e0dc204aefeb9616f403b3d2e3
bittu072 <bittu.patel072@gmail.com>
2016-12-10 03:23:24 -0800

    added web app that ask users for title and text input, if one of them is missing shows error msg and make input box blanks

---
+++ multiuserblog/base_pytohn.py
@@ -0,0 +1,28 @@
+# this is the general program, which can be used to display simple page # and can be edite to make complex website with the proper modification and addition
+
+import os
+
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a,**kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.write('xyz!!')
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
