d4b276bcfbcb07a34afc172564d03ee5cb18b2e0
bittu072 <bittu.patel072@gmail.com>
2016-12-10 04:02:45 -0800

    simple web app for ascii characters + google app datastore(database)

--- multiuserblog/project/ReferenceWorks/asciic.py
+++ multiuserblog/project/ReferenceWorks/asciic.py
@@ -1,9 +1,15 @@
 # simple implementation of variable with jinja2
 # with title and input text box
+# datatype for the database (in google app datastore) - int, float, string, text, date, time, datetime
+# properties in the google app datastore - email, link , postal address
+
 import os
 
 import jinja2
 import webapp2
+
+# for the database
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
@@ -20,9 +26,17 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+    title = db.StringProperty(required = True) # required=True means this can't be blank, here
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render("home.html", title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * from Art ORDER BY created desc")
+
+        self.render("home.html", title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render_front()
@@ -32,7 +46,9 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thanks!!!")
+            a = Art(title = title, art = art)
+            a.put()
+            self.redirect("/")
         else:
             error = "Something is Missing"
             self.render_front(title, art, error)
