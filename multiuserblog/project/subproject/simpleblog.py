3a19c7e69e6a10f6db05fec039d4c477b3410870
bittu072 <bittu.patel072@gmail.com>
2016-12-11 23:27:23 -0800

    created blogs

--- multiuserblog/project/subproject/simpleblog.py
+++ multiuserblog/project/subproject/simpleblog.py
@@ -1,85 +1,83 @@
-# simple implementation of variable with jinja2
-# with title and input text box
-# datatype for the database (in google app datastore) - int, float, string, text, date, time, datetime
-# properties in the google app datastore - email, link , postal address
-
 import os
+import re
+from string import letters
 
-import jinja2
 import webapp2
+import jinja2
 
-# for the database
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-class Handler(webapp2.RequestHandler):
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
-        self.response.write(*a,**kw)
+        self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Art(db.Model):
-    title = db.StringProperty(required = True) # required=True means this can't be blank, here
-    art = db.TextProperty(required = True)
+class MainPage(BlogHandler):
+  def get(self):
+      self.write('Welcome to the world of full-stack developer"')
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
-class MainPage(Handler):
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * from Art ORDER BY created desc")
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
 
-        self.render("home.html", title=title, art=art, error=error, arts=arts)
-
+class BlogFront(BlogHandler):
     def get(self):
-        self.render_front()
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('home.html', posts = posts)
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("blogpage.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
 
     def post(self):
-        title = self.request.get("title")
+        subject = self.request.get('subject')
+        content = self.request.get('content')
 
-
-class BlogCreatePage(Handler):
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * from Art ORDER BY created desc")
-        self.render("firstpage.html", title=title, art=art, error=error, arts=arts)
-
-    def render_post(self, title="", art=""):
-        arts = db.GqlQuery("SELECT * from Art ORDER BY created desc")
-        self.render("blogpage.html", title=title, art=art)
-
-    def get(self):
-        self.render_front()
-
-    def post(self):
-        title = self.request.get("title")
-        art = self.request.get("art")
-
-        if title and art:
-            a = Art(title = title, art = art)
-            a.put()
-            self.render_post(title, art)
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
         else:
-            error = "Something is Missing"
-            self.render_front(title, art, error)
-
-class Blog(Handler):
-    def get(self):
-        arts = db.GqlQuery("SELECT * from Art ORDER BY created desc")
-        n = self.request.get("n")
-        for i in arts:
-            if i.title==n:
-                arty=i.art
-                self.render("blogpage.html", title=n, art=arty)
-
-
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                            ('/new',BlogCreatePage),
-                            ('/blog',Blog)], debug=True)
+                               ('/blog/?', BlogFront),
+                               ('/blog/([0-9]+)', PostPage),
+                               ('/blog/newpost', NewPost),
+                               ],
+                              debug=True)
