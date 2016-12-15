499804ea8ed8136625f1c8735fe53574664d7b6f
bittu072 <bittu.patel072@gmail.com>
2016-12-14 23:55:14 -0800

    defined functions for creating the hash value for the cookie

--- multiuserblog/project/cookie_sample/samplecookie.py
+++ multiuserblog/project/cookie_sample/samplecookie.py
@@ -8,12 +8,33 @@ import os
 import jinja2
 import webapp2
 
+import hashlib
+
 # for the database
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+# Implemented the function make_secure_val, which takes a string and returns a
+# string of the format:
+# s,HASH
+def hash_str(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s,%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    a= h.split(",")
+    if h == make_secure_val(val):
+        return val
+    #or
+    # if hash_str(a[0])==a[1]:
+    #     return a[0]
+    # else:
+    #     return None
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
