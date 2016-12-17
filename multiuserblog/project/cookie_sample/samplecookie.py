29883077462c66f2131a3340b9988d943b35a673
bittu072 <bittu.patel072@gmail.com>
2016-12-16 22:50:32 -0800

    added password validation function

--- multiuserblog/project/cookie_sample/samplecookie.py
+++ multiuserblog/project/cookie_sample/samplecookie.py
@@ -45,13 +45,15 @@ def check_secure_val(h):
 def make_salt():
     return "".join(random.choice(string.letters) for x in xrange(5))
 
-def make_pw_hash(name, pw):
-    ###Your code here
-    salt = make_salt()
-    HASH = hashlib.sha256(name+pw+salt).hexdigest()
-    return "%s,%s" % (HASH, salt)
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name+pw+salt).hexdigest()
+    return "%s,%s" % (h, salt)
 
-print make_pw_hash("hello","hey"
+def valid_pw(name, pw, h):
+    salt=h.split(",")[1]
+    return h == make_pw_hash(name, pw, salt)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
