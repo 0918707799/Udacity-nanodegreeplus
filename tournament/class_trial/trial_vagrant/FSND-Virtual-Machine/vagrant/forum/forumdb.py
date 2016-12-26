54a1ae9552b112ffe4dc3d939e8003c37d404fd7
bittu072 <bittu.patel072@gmail.com>
2016-12-26 03:40:43 -0800

    added somw instructions

--- tournament/class_trial/trial_vagrant/FSND-Virtual-Machine/vagrant/forum/forumdb.py
+++ tournament/class_trial/trial_vagrant/FSND-Virtual-Machine/vagrant/forum/forumdb.py
@@ -4,6 +4,7 @@
 
 import time
 import psycopg2
+import bleach
 
 ## Database connection
 
@@ -21,6 +22,7 @@ def GetAllPosts():
     c = DB.cursor()
     c.execute("SELECT time, content FROM posts ORDER BY time DESC")
     posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+    bleach.clean(posts)
     DB.close()
     return posts
 
@@ -45,3 +47,28 @@ def AddPost(content):
     # c.execute("INSERT into posts (content) values ('%s')", % content)
     # and also if we add ''quotes then it will an error
     # so to avoid this above correction in code is the solution
+
+
+
+# This is simple spam code
+# <script>
+#     setTimeout(function() {
+#         var tt = document.getElementById('content');
+#         tt.value = "<h2 style='color: #FF6699; font-family: Comic Sans MS'>Spam, spam, spam, spam,<br>Wonderful spam, glorious spam!</h2>";
+#         tt.form.submit();
+#         }, 2500);
+# </script>
+#
+# your backend is done properly and server is securey done but this code can cause spam due to browser
+# your web server and data server is treating this as a text but browser is interpriting with different meaning
+#
+# this is called script injection attack
+# you can use python library named "Bleach" to get out of this scenario and save you web application form spam like this
+#
+# but data which has been already entered, you need to remove it or replace it
+# update posts set content='cheese' where content like '%spam%';
+# this update query on your database table will replace all damaged data done by script injection to cheese
+
+
+# To delete the post use following query
+# delete from posts where content='cheese';
