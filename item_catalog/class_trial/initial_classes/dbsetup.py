11b6d3b488c700984e65d76d7729089d27cd3a45
bittu072 <bittu.patel072@gmail.com>
2017-01-06 19:41:01 -0800

    added comments and added lines for ending the file

--- item_catalog/class_trial/initial_classes/dbsetup.py
+++ item_catalog/class_trial/initial_classes/dbsetup.py
@@ -1,3 +1,6 @@
+# begininning of the file
+# import all the required libraries
+
 import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -7,3 +10,15 @@ from sqlalchemy.ext.declarative import declareative_base
 from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine
+
+Base = declareative_base()
+
+
+
+
+
+# at the end of the file
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
