2b59a45c07830514b3d07c0045e12eb50aaf1796
bittu072 <bittu.patel072@gmail.com>
2017-01-16 22:02:59 -0800

    updated database file with added user class

--- item_catalog/vagrant/catalog/dbsetup.py
+++ item_catalog/vagrant/catalog/dbsetup.py
@@ -15,11 +15,21 @@ Base = declarative_base()
 
 # representation of table as a python class
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(250))
+    picture = Column(db.LargeBinary)
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
 class MenuItem(Base):
@@ -32,6 +42,8 @@ class MenuItem(Base):
     price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     # This serialize function is to send JSON objects in a
     # serializable format
