18b72e5d5b1f1df410d6cd33b9cd1d43b018cc3c
bittu072 <bittu.patel072@gmail.com>
2017-01-30 21:00:40 -0800

    added code that grabs image from google street api and shows in the background of web page

--- Neighborhood Map/class_trial/initia_classes/Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
+++ Neighborhood Map/class_trial/initia_classes/Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
@@ -10,7 +10,17 @@ function loadData() {
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
+    // grab the value of input box with id="street"
+    var streetStr = $('#street').val();
+    // grab the value of input box with id="city"
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
 
+    // to print something in the top
+    $greeting.text('So, you have decided new address?');
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+
+    $("body").append('<img class="bgimg" src="' +streetviewUrl + '">');
     // load streetview
 
     // YOUR CODE GOES HERE!
