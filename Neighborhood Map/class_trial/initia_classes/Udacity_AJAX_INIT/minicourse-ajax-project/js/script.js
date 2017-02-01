e0d5abcc3a610ebd73b2b29463fe3f395783718f
bittu072 <bittu.patel072@gmail.com>
2017-01-31 22:46:26 -0800

    added more spacing in print statements

--- Neighborhood Map/class_trial/initia_classes/Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
+++ Neighborhood Map/class_trial/initia_classes/Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
@@ -14,10 +14,10 @@ function loadData() {
     var streetStr = $('#street').val();
     // grab the value of input box with id="city"
     var cityStr = $('#city').val();
-    var address = streetStr + ',' + cityStr;
+    var address = streetStr + ', ' + cityStr;
 
     // to print something in the top
-    $greeting.text('So, you have decided new address?');
+    $greeting.text('So, you have decided new address "' + address + '"?');
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
 
     $("body").append('<img class="bgimg" src="' +streetviewUrl + '">');
