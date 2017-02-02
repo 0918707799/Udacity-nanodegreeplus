659106454f946db42f2353f5f167eb3eba4ddd7b
bittu072 <bittu.patel072@gmail.com>
2017-02-02 00:10:56 -0800

    added second dog and dogs' name+added css

--- Neighborhood Map/class_trial/dog_clicker/js/dogscript.js
+++ Neighborhood Map/class_trial/dog_clicker/js/dogscript.js
@@ -1,6 +1,17 @@
 var i=0;
-$( "img" ).click(function() {
-  var $num = $('#num');
+var dog1="RUFFY";
+$("#name1").text(dog1);
+$( "#image1" ).click(function() {
+  var $num1 = $('#num1');
   i = i+1;
-  $num.text("_______________ " +i+ "________");
+  $num1.text("Click-Count is: " +i+ "________");
+});
+
+var j=0;
+var dog2="SCOOBY";
+$("#name2").text(dog2);
+$( "#image2" ).click(function() {
+  var $num2 = $('#num2');
+  j = j+1;
+  $num2.text("Click-Count is: " +j+ "________");
 });
