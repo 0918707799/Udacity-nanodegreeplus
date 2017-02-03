c75bc31654a511da4931b597425538086c8820a5
bittu072 <bittu.patel072@gmail.com>
2017-02-03 01:24:51 -0800

    added list of dogs + can store separate clickcount of all the images

--- Neighborhood Map/class_trial/dog_clicker/js/dogscript.js
+++ Neighborhood Map/class_trial/dog_clicker/js/dogscript.js
@@ -1,17 +1,36 @@
-var i=0;
-var dog1="RUFFY";
-$("#name1").text(dog1);
-$( "#image1" ).click(function() {
-  var $num1 = $('#num1');
-  i = i+1;
-  $num1.text("Click-Count is: " +i+ "________");
+var dognum = [0,0,0,0,0,0,0];
+var dogobj ={};
+
+var $name = $('#name');
+var $num = $('#num');
+// var $img = $('#image');
+
+
+
+$("#dog li").click(function() {
+    $name.text($(this).text());
+    id = Number(this.id);
+    var imgName = 'img/img'+id+'.jpg';
+    // $num.text(imgName);
+    dogobj[imgName] = id;
+    // $("#imghld").append('<img src="' +imgName + '">');
+    $( "#imghld > img" ).replaceWith('<img src="' +imgName + '">');
+
 });
 
-var j=0;
-var dog2="SCOOBY";
-$("#name2").text(dog2);
-$( "#image2" ).click(function() {
-  var $num2 = $('#num2');
-  j = j+1;
-  $num2.text("Click-Count is: " +j+ "________");
+function getProperty(propertyName) {
+    return dogobj[propertyName];
+};
+
+// Example of getProperty function
+// getProperty("key1");
+
+
+$( "#imghld" ).click(function() {
+    var img = $(this).find("img");
+    var img_path = img.attr("src");
+    var $num = $('#num');
+    var dogid = dogobj[img_path];
+    dognum[dogid] = dognum[dogid]+1;
+    $num.text("ClickCount is :: " + dognum[dogid]);
 });
