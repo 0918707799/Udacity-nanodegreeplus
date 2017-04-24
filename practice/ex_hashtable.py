7be6ae2d319040f4c7b2d2723693c3d90ea0cd18
bittu072 <bittu.patel072@gmail.com>
2017-04-23 23:12:37 -0700

    added another method of hashtable

--- practice/ex_hashtable.py
+++ practice/ex_hashtable.py
@@ -8,10 +8,10 @@ class HashTable(object):
     def __init__(self):
         self.table = [None]*10000
         self.dicty = {}
-        
+
 
     def store(self, string):
-        """Input a string that's stored in 
+        """Input a string that's stored in
         the table."""
         self.dicty[self.calculate_hash_value(string)] = string
         pass
@@ -37,7 +37,34 @@ class HashTable(object):
             counter =+ 1
         has_val = (ord(a[0])*100) + ord(a[1])
         return has_val
-    
+
+    # OR
+    # solution provided by udacity
+    # 
+    # def __init__(self):
+    #     self.table = [None]*10000
+    #
+    # def store(self, string):
+    #     hv = self.calculate_hash_value(string)
+    #     if hv != -1:
+    #         if self.table[hv] != None:
+    #             self.table[hv].append(string)
+    #         else:
+    #             self.table[hv] = [string]
+    #
+    # def lookup(self, string):
+    #     hv = self.calculate_hash_value(string)
+    #     if hv != -1:
+    #         if self.table[hv] != None:
+    #             if string in self.table[hv]:
+    #                 return hv
+    #     return -1
+    #
+    # def calculate_hash_value(self, string):
+    #     value = ord(string[0])*100 + ord(string[1])
+    #     return value
+
+
 # Setup
 hash_table = HashTable()
 
@@ -58,4 +85,3 @@ print hash_table.lookup('UDACITY')
 hash_table.store('UDACIOUS')
 # Should be 8568
 print hash_table.lookup('UDACIOUS')
-
