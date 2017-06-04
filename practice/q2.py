f581961ba45c0386b1c9d91c39efa46821be8f24
bittu072 <bittu.patel072@gmail.com>
2017-06-04 01:27:41 -0700

    added program to find the largest palindromic substring from the string if any

---
+++ practice/q2.py
@@ -0,0 +1,47 @@
+# Question 2
+# Given a string a, find the longest palindromic substring contained in a.
+# Your function definition should look like question2(a), and return a string.
+
+# NOTE:
+# if string has two similar length palindromic substring then function will return the first substring
+
+
+def question2(a):
+    length = len(a)
+    start = -1
+    end = -1
+    final_end = -1
+    final_start = -1
+    diff = 0
+
+    if length == 1:
+        return a
+    for i in range(0,length):
+        if i > (length/2):
+            trial = length - i
+        else:
+            trial = i
+        for j in range(1, trial):
+            if (i-j) <= 0 or (i+j) >= length or (j-i) == (j+i):
+                continue
+            else:
+                if a[i-j] == a[i+j]:
+                    start = i-j
+                    end = i+j
+        if (end-start) > diff:
+            diff = end - start
+            final_end = end
+            final_start = start
+    if diff == 0:
+        return None
+    else:
+        return a[final_start:final_end+1]
+
+# ans should be "qwertytrewq"
+print question2("abqwertytrewqcdeyeheyeye")
+
+#ans should be "eye"
+print question2("abeyej")
+
+
+# TODO: Try to solve this question without two for loop
